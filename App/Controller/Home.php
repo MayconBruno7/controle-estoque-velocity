55985b8b8e6ee8c87f34bea4f3b0e5c4f5b36a64
Maykin-7 <123507156+Maykin-7@users.noreply.github.com>
2024-05-30 11:37:37 -0300

    Passando para o framework

---
+++ App/Controller/Home.php
@@ -0,0 +1,78 @@
+<?php
+
+use App\Library\ControllerMain;
+
+class Home extends ControllerMain
+{
+    /**
+     * index
+     *
+     * @return void
+     */
+    public function index()
+    {
+        // $categoria = $this->loadModel("Categoria");
+        // $this->dados['aCategoria'] = $categoria->lista();
+    
+        // $this->loadView("home", $this->dados);
+
+        $this->loadView("usuario/login", $this->dados);
+
+    }
+
+    /**
+     * produto
+     *
+     * @return void
+     */
+    public function produto()
+    {
+        $CategoriaModel = $this->loadModel("categoria");
+
+        $aCategoria = $CategoriaModel->lista();
+
+        return $this->loadView("produto", $aCategoria);
+    }
+
+    /**
+     * contato
+     *
+     * @return void
+     */
+    public function contato()
+    {
+        $this->loadView("contato");
+    }
+
+    /**
+     * login
+     *
+     * @return void
+     */
+    public function login()
+    {
+        return $this->loadView('usuario/login');
+    }
+
+    /**
+     * homeAdmin
+     *
+     * @return void
+     */
+    public function homeAdmin()
+    {
+        return $this->loadView("restrita/homeAdmin");
+    }
+
+    /**
+     * criarConta
+     *
+     * @return void
+     */
+    public function criarConta()
+    {
+        $this->loadHelper('Formulario');
+        
+        return $this->loadView("usuario/formCriarConta", []);
+    }
+}
--- limparSessao.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    session_start();
-
-    // Limpa a sessão movimentacao
-    unset($_SESSION['movimentacao']);
-    // limpa a sessão de produtos
-    unset($_SESSION['produtos']);
-?>
